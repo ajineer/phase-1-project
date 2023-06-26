1b1817ecd4e191906c9f5cd796e14aed2698015e
Mark <ajineer@outlook.com>
2023-06-25 19:10:38 -0600

    refactored code, made patch items function to move incomplete items to complete items

--- index.js
+++ index.js
@@ -5,35 +5,109 @@ function initialized(){
     .then(res => res.json())
     .then(data => {
         Lists = data
-        //loadListDetails(Lists[0])
         Lists.forEach(list => {
-            loadListName(list);
-        });
+            //loadListName(list)
+            let toDoLists = document.querySelector("#lists")
+            let listItem = createListsItem(list.name)
+            toDoLists.appendChild(listItem)
+            listItem.addEventListener("click", ()=>{
+                loadListItems(list.incomplete, list.complete, list.name)
+                let domICitems = document.querySelector("#incomplete-items").querySelectorAll("li")
+                domICitems.forEach(icItem =>{
+                    icItem.querySelector("#checkbox").addEventListener("click", ()=>{
+                        let updatedTasks = updateICtasks(icItem.querySelector("span").innerText, list)
+                        updateIClist(list, updatedTasks)
+                    })
+                })
+            })
+        })    
     })
 
-    function loadListName(aList){
-        let toDoLists = document.querySelector("#lists")
-        let listItem = createListsItem(aList.name)
-        
-        toDoLists.appendChild(listItem)
-        let editButton = listItem.querySelector("#edit")
-        
-        editButton.addEventListener("click", (e) =>{
-            listNameSpan = listItem.querySelector("span")
-            listNameSpan.style.backgroundColor = "white"
-            listNameSpan.contentEditable = true
-            listNameSpan.focus();
-            listNameSpan.addEventListener("keydown", event =>{
-                if(event.key === "Enter"){
-                    event.preventDefault()
-                    listNameSpan.contentEditable = false
-                    listNameSpan.style.backgroundColor = ""
-                    patchListName(aList.id, listNameSpan.innerText)
-                }
+    function updateIClist(aList, tasks){
+        fetch(`http://localhost:3000/Lists/${aList.id}`,{
+            method:"PATCH",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                id: aList.id,
+                name: aList.name,
+                incomplete: tasks[0],
+                complete: tasks[1]
             })
+        })
+        .then(res => res.json())
+        .then(patchedList => loadListItems(patchedList.incomplete, patchedList.complete, patchedList)
+        )
+    }
+
+        // let editButton = listItem.querySelector("#edit")
+        // let deleteButton = listItem.querySelector("#deleteList")
+
+        // deleteButton.addEventListener("click", () => {
+        //     deleteList(aList.id)
+        //     listItem.remove()
+        // }) 
+
+        // editButton.addEventListener("click", (e) =>{
+        //     listNameSpan = listItem.querySelector("span")
+        //     listNameSpan.style.backgroundColor = "white"
+        //     listNameSpan.contentEditable = true
+        //     listNameSpan.focus();
+        //     listNameSpan.addEventListener("keydown", event =>{
+        //         if(event.key === "Enter"){
+        //             event.preventDefault()
+        //             listNameSpan.contentEditable = false
+        //             listNameSpan.style.backgroundColor = ""
+        //             patchListName(aList.id, listNameSpan.innerText)
+        //         }
+        //     })
+        // })
+
+        // listItem.addEventListener("click", ()=>{
+        //     loadListItems(aList.incomplete, aList.complete, aList.name)
+        //     let incompleteTaskList = document.querySelector("#incomplete-items").querySelectorAll("li")
+        //     incompleteTaskList.forEach(task => {
+        //         task.querySelector("#checkbox").addEventListener("click", ()=>{
+        //             let updatedTasks = updateICtasks(task, aList)
+        //             loadListItems(updatedTasks[0], updatedTasks[1], aList.name)
+        //         })
+        //     })
+        // })
+
+    function updateICtasks(taskText, aList){
+        let icIndex = aList.incomplete.indexOf(taskText)
+        aList.complete.push(taskText)
+        aList.incomplete.splice(icIndex, 1)
+        return [aList.incomplete, aList.complete]
+    }
+
+    function loadListItems(incompleteList, completeList, listName){
+        deleteListDetails();
+        let alistName = document.querySelector("#name")
+        alistName.innerText = listName
+        let incompleteDOMList = document.querySelector("#incomplete-items")
+        let completeDOMList = document.querySelector("#complete-items")
+        incompleteList.forEach(item =>{
+            let incompleteTask = createListItem(item)
+            incompleteDOMList.appendChild(incompleteTask)
+        })
+        completeList.forEach(item =>{
+            let completeTask = createListItem(item)
+            completeDOMList.appendChild(completeTask)
+        })  
+    }
+    function deleteList(listId){
+        fetch(`http://localhost:3000/Lists/${listId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
         })
     }
     function patchListName(anId, aListName){
+        
         fetch(`http://localhost:3000/Lists/${anId}`, {
             method: "PATCH",
             headers: {
@@ -44,7 +118,6 @@ function initialized(){
                 name: aListName
             })
         })
-
     }
     
     function createListsItem(listName){
@@ -58,7 +131,6 @@ function initialized(){
         listsItem.append(listsSpan, editButton)
         
         return listsItem
-
     }
 
     function createListItem(appendText){
@@ -74,12 +146,15 @@ function initialized(){
         
         theItem.appendChild(textSpan)
         theItem.appendChild(buttons)
-        
+
         return theItem
     }
+
     function deleteListDetails(){
-        let currentToDoList = document.querySelector("#items")
-        currentToDoList.innerHTML = "<ul id=\"items\"></ul>"
+        let incompleteList = document.querySelector("#incomplete-items")
+        let completeList = document.querySelector("#complete-items")
+        incompleteList.innerHTML = "<ul id=\"incomplete-items\"></ul>"
+        completeList.innerHTML = "<ul id=\"complete-items\"></ul>"
     }
 }
 
