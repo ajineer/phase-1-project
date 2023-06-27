a453a92f73c2c457d8ccf8f03c0cbcffba26e954
Mark <ajineer@outlook.com>
2023-06-27 16:27:32 -0600

    added edit feature to tasks

--- index.js
+++ index.js
@@ -10,30 +10,19 @@ function initialized(){
         })    
         loadListDetail(Lists[0])
         createNewList()
+        createNewItem()
     })
 
-    function createNewItem(aList){
+    function createNewItem(){
         document.querySelector("#new-item-form").addEventListener("submit", (e)=>{
             e.preventDefault()
             if(e.target[0].value !==""){
-                aList.incomplete.push(e.target[0].value)
-                fetch(`http://localhost:3000/Lists/${aList.id}`, {
-                    method: "PATCH",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify({
-                        id: aList.id,
-                        name: aList.name,
-                        incomplete: aList.incomplete,
-                        complete: aList.complete
-                    })
-                })
-                loadListDetail(aList)
+                currentlist.incomplete.push(e.target[0].value)
+                patchTasks(currentlist, [currentlist.incomplete, currentlist.complete])
+                loadListDetail(currentlist)
             }else{
                 alert("Please enter a new task!")
-            }
+            } 
             e.target.reset()
         })
     }
@@ -64,11 +53,12 @@ function initialized(){
             }
         })
     }
+
     function loadLists(aList){
         let toDoLists = document.querySelector("#lists")
         let listItem = createListsItem(aList.name)
         toDoLists.appendChild(listItem)
-        listItem.addEventListener("click", ()=>{
+        listItem.addEventListener("click", (e)=>{
             loadListDetail(aList)
         })
     }
@@ -84,49 +74,73 @@ function initialized(){
         document.querySelector("#name").innerText = aList.name
         renderTasks(tasksLi[0], tasksLi[1])
         addCBEventListeners(tasksLi, currentlist)
-        document.querySelector("#new-item-form").addEventListener("submit", (e)=>{
-            e.preventDefault()
-            if(e.target[0].value !==""){
-                currentlist.incomplete.push(e.target[0].value)
-                fetch(`http://localhost:3000/Lists/${currentlist.id}`, {
-                    method: "PATCH",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify({
-                        id: currentlist.id,
-                        name: currentlist.name,
-                        incomplete: currentlist.incomplete,
-                        complete: currentlist.complete
-                    })
-                }).then(res => res.json())
-                .then(data => {
-                    loadListDetail(data)
-                })
-            }else{
-                alert("Please enter a new task!")
-            }
-            e.target.reset()
-        })
     }
 
-    function addCBEventListeners(aTaskList, currentlist){
+    function addCBEventListeners(aTaskList, aList){
         aTaskList[0].forEach(li =>{
-            li.querySelector("#checkbox").addEventListener("click", ()=>{
-                let updatedICtasks = updateICtasks(li.querySelector("span").innerText, currentlist)
-                patchTasks(currentlist, updatedICtasks)
-                loadListDetail(currentlist)
+            let checkButton = li.querySelector("#checkbox")
+            let editButton = li.querySelector("#editTask")
+
+            checkButton.addEventListener("click", ()=>{
+                let updatedTasks = updateTasks(li.querySelector("span").innerText, aList, true)
+                patchTasks(aList, updatedTasks)
+                loadListDetail(aList)
+            })
+
+            editButton.addEventListener("click", ()=>{
+                let textSpanElement = li.querySelector("span")
+                let textSpanValue = textSpanElement.innerText
+                textSpanElement.style.backgroundColor = "white"
+                textSpanElement.contentEditable = true
+                textSpanElement.addEventListener("keydown", (e)=>{
+                    if(e.key === "Enter"){
+                        textSpanElement.style.backgroundColor = ""
+                        textSpanElement.contentEditable = false
+                        let editedTasks = editTask(aList, textSpanValue, e, true)
+                        patchTasks(currentlist, editedTasks)
+                    }
+                })     
             })
         })
         aTaskList[1].forEach(li =>{
-            li.querySelector("#checkbox").addEventListener("click", ()=>{
-                let updatedCtasks = updateCtasks(li.querySelector("span").innerText, currentlist)
-                patchTasks(currentlist, updatedCtasks)
+            let checkButton = li.querySelector("#checkbox")
+            let editButton = li.querySelector("#editTask")
+
+            checkButton.addEventListener("click", ()=>{
+                let updatedTasks = updateTasks(li.querySelector("span").innerText, currentlist, false)
+                patchTasks(currentlist, updatedTasks)
                 loadListDetail(currentlist)
+            })
+
+            editButton.addEventListener("click", ()=>{
+                let textSpanElement = li.querySelector("span")
+                let textSpanValue = textSpanElement.innerText
+                textSpanElement.style.backgroundColor = "white"
+                textSpanElement.contentEditable = true
+                textSpanElement.addEventListener("keydown", (e)=>{
+                    if(e.key === "Enter"){
+                        textSpanElement.style.backgroundColor = ""
+                        textSpanElement.contentEditable = false
+                        let editedTasks = editTask(aList, textSpanValue, e, false)
+                        console.log(editedTasks)
+                        patchTasks(currentlist, editedTasks)
+                    }
+                })     
             })
         })
     }
+
+    function editTask(aList, currentText, event, isIC){
+        isIC ? (
+            aList.incomplete = aList.incomplete.filter(icTask => icTask !== currentText),
+            aList.incomplete.push(event.target.innerText)
+        ):(
+            aList.complete = aList.complete.filter(cTask => cTask !== currentText),
+            aList.complete.push(event.target.innerText)
+        )
+        return [aList.incomplete, aList.complete]
+    }
+
     function patchTasks(aList, taskList){
         
         fetch(`http://localhost:3000/Lists/${aList.id}`, {
@@ -143,17 +157,14 @@ function initialized(){
             })
         })
     }
-    function updateICtasks(liTask, aList){
-        let icIndex = aList.incomplete.indexOf(liTask)
-        aList.complete.push(liTask)
-        aList.incomplete.splice(icIndex, 1)
-        return [aList.incomplete, aList.complete]
-    }
 
-    function updateCtasks(liTask, aList){
-        let cIndex = aList.complete.indexOf(liTask)
-        aList.incomplete.push(liTask)
-        aList.complete.splice(cIndex, 1)
+    function updateTasks(liTask, aList, taskIC){
+        taskIC ? (
+            aList.complete.push(liTask), 
+            aList.incomplete = aList.incomplete.filter(tasks => tasks !== liTask)):(
+            aList.incomplete.push(liTask),
+            aList.complete = aList.complete.filter(tasks => tasks !== liTask)
+        ) 
         return [aList.incomplete, aList.complete]
     }
 
@@ -166,7 +177,7 @@ function initialized(){
         textSpan.id = "itemText"
         textSpan.innerText = appendText
         buttons.id = "buttons-container"
-        buttons.innerHTML = "<button id=\"checkbox\"><i>&#10003;</i></button>"
+        buttons.innerHTML = "<button id=\"editTask\"><i>&#9998;</i></button><button id=\"checkbox\"><i>&#10003;</i></button>"
         
         theItem.appendChild(textSpan)
         theItem.appendChild(buttons)
@@ -175,6 +186,7 @@ function initialized(){
     }
 
     function renderTasks(domIncompleteTasks, domcompletedTasks){
+
         let incompleteDOMtasks = document.querySelector("#incomplete-items")
         let completeDOMtasks = document.querySelector("#complete-items")
         
@@ -187,15 +199,18 @@ function initialized(){
     }
 
     function deleteListDetails(){
+
         let incompleteList = document.querySelector("#incomplete-items")
         let completeList = document.querySelector("#complete-items")
         let listName = document.querySelector("#name")
+        
         listName.innerText = ""
         incompleteList.innerHTML = "<ul id=\"incomplete-items\"></ul>"
         completeList.innerHTML = "<ul id=\"complete-items\"></ul>"
     }
 
     function createListsItem(listName){
+
         let listsItem = document.createElement("li")
         let listsSpan = document.createElement("span")
 
@@ -205,6 +220,5 @@ function initialized(){
         return listsItem
     }
 }
-
 
 document.addEventListener("DOMContentLoaded", initialized())
